50265e229b5485a213278e93a2db19c627151220
Alexis Perez <alexisperez@4gmail.com>
2021-07-04 04:16:29 -0400

    configurando db local y produccion parte 2

--- src/database/database.js
+++ src/database/database.js
@@ -11,19 +11,7 @@ const config_db = process.env.NODE_ENV !== 'production' ?
     }
   };
 
-const pool = new Pool(config_db);
+  const pool = new Pool(config_db);
 
-
-// const { Pool } = require('pg');
-
-// const config_db = {
-//     user: process.env.DATABASE_USER,
-//     host: process.env.DATABASE_HOST,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_NAME,
-//     port: process.env.DATABASE_PORT
-// };
-
-// const pool = new Pool(config_db)
 
 module.exports = pool;
