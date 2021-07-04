e9c4f6c100a44089c0ee9971fc84d752182ae79b
Alexis Perez <alexisperez@4gmail.com>
2021-07-04 03:32:02 -0400

    coneccion de la base de datos para heroku

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,22 @@
 const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-const config_db = {
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT
-};
 
-const pool = new Pool(config_db)
+// const { Pool } = require('pg');
+
+// const config_db = {
+//     user: process.env.DATABASE_USER,
+//     host: process.env.DATABASE_HOST,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE_NAME,
+//     port: process.env.DATABASE_PORT
+// };
+
+// const pool = new Pool(config_db)
 
 module.exports = pool;
