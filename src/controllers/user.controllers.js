b26808a5614ec5789504676dfff4f417fbcac327
Alexis Perez <alexisperez@4gmail.com>
2021-07-04 22:17:40 -0400

    traduciendo mensajes de alerta al español

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -14,10 +14,10 @@ usersCtrl.signup = async (req, res) => {
     const { name, email, password, confirm_password } = req.body;
     const errors = [];
     if (password != confirm_password) {
-        errors.push({ text: 'Passwords do not match' })
+        errors.push({ text: 'La contraseña no coinciden.' })
     }
     if (password.length < 4) {
-        errors.push({ text: 'Passwords must be at last 4 characters.' });
+        errors.push({ text: 'Las contraseñas deben tener al menos 4 caracteres.' });
     }
     if (errors.length > 0) {
         res.render('users/signup', {
